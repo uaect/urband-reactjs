8cd0cd6c3211cd72777ae98ae38a5e78baf388ad
ijas <ijas.prof@gmail.com>
2019-11-13 17:59:56 +0400

    ijas

--- src/components/Pages/slider/slider.component.jsx
+++ src/components/Pages/slider/slider.component.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import * as actionCreators from "../../../../src/store/actions/";
-import {
-  CarouselProvider,
-  Slider,
-  Slide
-} from "pure-react-carousel";
+import { CarouselProvider, Slider, Slide } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
 class sliderHero extends React.Component {
-   state = {
+  state = {
     backgroundImage: []
-  }
+  };
   componentDidMount() {
     this.props.fetchHomeBanner("Home");
   }
@@ -18,9 +14,9 @@ class sliderHero extends React.Component {
   render() {
     const image_url = "https://admin.urbandmusic.com/storage/";
     const getbanner = this.props.homebanners;
-  if(getbanner){
-    var total = getbanner.length;
-  }
+    if (getbanner) {
+      var total = getbanner.length;
+    }
 
     return (
       <div className="heroCarouselBox">
@@ -32,38 +28,44 @@ class sliderHero extends React.Component {
           playDirection="forward"
           touchEnabled={true}
           isPlaying={true}
-          interval = {9000}
+          interval={7000}
         >
           <Slider>
-          {getbanner && getbanner.map(item => {
-              return (
-            <Slide index={0}>
-              <section className="banner-five" style={{ backgroundImage: `url(${image_url + item.image})` }}>
-                <div className="tim-container">
-                  <div className="baneer-five-content">
-                    <div className="content sp-container">
-                      <div className="sp-content">
-                        <div className="sp-globe"></div>
-                        <h2 className="frame-1">{item.title}</h2>
-                        <h2 className="frame-2">{item.title}</h2>
-                        <h2 className="frame-2">{item.title}</h2>
-                        <h2 className="frame-2">{item.title}</h2>
+            {getbanner &&
+              getbanner.map(item => {
+                return (
+                  <Slide index={0}>
+                    <section
+                      className="banner-five"
+                      style={{
+                        backgroundImage: `url(${image_url + item.image})`
+                      }}
+                    >
+                      <div className="tim-container">
+                        <div className="baneer-five-content">
+                          <div className="content sp-container">
+                            <div className="sp-content">
+                              <div className="sp-globe"></div>
+                              <h2 className="frame-1">{item.title}</h2>
+                              <h2 className="frame-2">{item.title}</h2>
+                              <h2 className="frame-3">{item.title}</h2>
+                              <h2 className="frame-4">{item.title}</h2>
+                            </div>
+                            <h3>{item.sub_title}</h3>
+                            <a className="tim-slide-btn" href="index.html#">
+                              TICKETS
+                            </a>
+                          </div>
+                        </div>
                       </div>
-                      <h3>{item.sub_title}</h3>
-                      <a className="tim-slide-btn" href="index.html#">
-                        TICKETS
-                      </a>
-                    </div>
-                  </div>
-                </div>
 
-                <div className="smoke-wrqpper">
-                  <canvas id="canvas"></canvas>
-                </div>
-              </section>
-            </Slide>);
-            })}
-
+                      <div className="smoke-wrqpper">
+                        <canvas id="canvas"></canvas>
+                      </div>
+                    </section>
+                  </Slide>
+                );
+              })}
           </Slider>
         </CarouselProvider>
       </div>
@@ -76,7 +78,7 @@ class sliderHero extends React.Component {
 }
 const mapDispatchToProps = dispatch => {
   return {
-    fetchHomeBanner: (id) => dispatch(actionCreators.fetchHomeBanner(id))
+    fetchHomeBanner: id => dispatch(actionCreators.fetchHomeBanner(id))
   };
 };
 
@@ -86,4 +88,7 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(sliderHero);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(sliderHero);
