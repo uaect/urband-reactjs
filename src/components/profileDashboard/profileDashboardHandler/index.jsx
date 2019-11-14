30ed29eafd1cfb0baff6d1512e5bd545930fa04b
Shejin Abu <shejin.abu@gmail.com>
2019-11-14 14:27:06 +0400

    ijas

--- src/components/profileDashboard/profileDashboardHandler/index.jsx
+++ src/components/profileDashboard/profileDashboardHandler/index.jsx
@@ -1,27 +1,166 @@
-import React, { Component } from "react";
-class ProfileDashboard extends Component {
-  render() {
-    return (
-      <div className="profileDashBoardBox">
-        <div>
-          <div className="tim-container clearfix">
-            <div className="ProfileBoardContent">
-              <div className="DsplayFlexBoxTp1">
-                <div className="LftBox">
-                  <div>
-                    <div className="lftPrfDesc1">
-                      <div className="DescTxtTp1">Hala Ijas!</div>
-                      <div className="DescTxtTp2">Sign Out</div>
-                    </div>
-                  </div>
-                </div>
-                <div className="RgtBox">Ijas</div>
-              </div>
-            </div>
-          </div>
-        </div>
+// import React, { Component } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link,
+//   useParams,
+//   useRouteMatch
+// } from "react-router-dom";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faUserAlt,faBabyCarriage,faBreadSlice
+// } from "@fortawesome/free-solid-svg-icons";
+// import ProfileEntryBox from '../profileEntryBox/profileEntryBox';
+// class ProfileDashboard extends Component {
+//   render() {
+//     let { path, url } = useRouteMatch();
+//     return (
+//       <div className="profileDashBoardBox">
+//         <div>
+//           <div className="tim-container clearfix">
+//             <div className="ProfileBoardContent">
+//               <div className="DsplayFlexBoxTp1">
+//                 <div className="LftBox">
+//                   <div>
+//                     <div className="lftPrfDesc1">
+//                       <div className="DescTxtTp1">Hala Ijas!</div>
+//                       <div className="DescTxtTp2">Sign Out</div>
+//                     </div>
+//                     <div className="PrfLinkBoxTp1">
+//                       <div className="ProfileNavItem">
+//                         <a href="#">
+//                         <FontAwesomeIcon icon={faUserAlt} className="prfIcnTp1"/>Profile
+//                         </a>
+//                       </div>
+//                       <div className="ProfileNavItem">
+//                         <a href="#">
+//                         <FontAwesomeIcon icon={faBreadSlice} className="prfIcnTp1"/>Addresses
+//                         </a>
+//                       </div>
+//                       <div className="ProfileNavItem">
+//                         <a href="#">
+//                         <FontAwesomeIcon icon={faBabyCarriage} className="prfIcnTp1"/>orders
+//                         </a>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div className="RgtBox">
+//                   <Router>
+//                     <Switch>
+//                       <Route exact path="/" component={ProfileEntryBox}/>
+//                       <Route path="/users" component={ProfileEntryBox}/>
+//                     </Switch>
+//                   </Router>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default ProfileDashboard;
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
+
+// Since routes are regular React components, they
+// may be rendered anywhere in the app, including in
+// child elements.
+//
+// This helps when it's time to code-split your app
+// into multiple bundles because code-splitting a
+// React Router app is the same as code-splitting
+// any other React app.
+
+export default function NestingExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
-export default ProfileDashboard;
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Topics() {
+  // The `path` lets us build <Route> paths that are
+  // relative to the parent route, while the `url` lets
+  // us build relative links.
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${url}/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  // The <Route> that rendered this component has a
+  // path of `/topics/:topicId`. The `:topicId` portion
+  // of the URL indicates a placeholder that we can
+  // get from `useParams()`.
+  let { topicId } = useParams();
+
+  return (
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
---
+++ src/components/profileDashboard/profileEntryBox/profileEntryBox.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class ProfileEntryBox extends Component {
+    render(){
+        return(
+            <div>Ijas</div>
+        )
+    }
+}
+export default ProfileEntryBox;
