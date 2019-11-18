d612846d0bdb0a4a9b9c16187bf7e6a7c94db86f
ijas <ijas.prof@gmail.com>
2019-11-18 10:30:52 +0400

    ijas Detail

--- src/components/Pages/PreviousShows/previousShow.jsx
+++ src/components/Pages/PreviousShows/previousShow.jsx
@@ -1,129 +1,147 @@
-
-import React, { Component } from 'react'
-import ModalVideo from '../../VideoModal/video-modal.component';
-import './style.css'
+import React, { Component } from "react";
+import ModalVideo from "../../VideoModal/video-modal.component";
+import "./style.css";
 import { connect } from "react-redux";
 import * as actionCreators from "../../../../src/store/actions/";
 class previousShowHome extends Component {
-	componentDidMount() {
-		this.props.previousshow();
-	}
+  componentDidMount() {
+    this.props.previousshow();
+  }
 
-	render() {
-		const image_url = "https://admin.urbandmusic.com/storage/";
-		const previous = this.props.previous;
-		var imageArray = [];
-		var videoArray = [];
-		if (previous.video_url) {
-			var videoo = previous.video_url.split('v=').map(function (videoo, index) {
-			  videoArray.push(videoo)
-	
-			});
-		  }
-		  console.log("vodeoarray", videoArray[1] );
-		  
-		if(previous.images){
-		var imagess = previous.images.split(',').map(function (imagess, index) {
-			imageArray.push(imagess)
-		
-			
-		});
-	}
+  render() {
+    const image_url = "https://admin.urbandmusic.com/storage/";
+    const previous = this.props.previous;
+    var imageArray = [];
+    var videoArray = [];
+    if (previous.video_url) {
+      var videoo = previous.video_url.split("v=").map(function(videoo, index) {
+        videoArray.push(videoo);
+      });
+    }
+    console.log("vodeoarray", videoArray[1]);
 
-	
+    if (previous.images) {
+      var imagess = previous.images.split(",").map(function(imagess, index) {
+        imageArray.push(imagess);
+      });
+    }
 
-		return (
+    return (
+      <section className="show-archive">
+        {previous ? (
+          <div className="container">
+            <div className="d-flex justify-content-center row">
+              <div className="col-xl-10">
+                <div className="section-title style-five">
+                  <h2>PREVIOUS SHOWS</h2>
+                  <p>{previous.title ? previous.title : ""}</p>
+                </div>
+                <div className="show-archive-wrapper row no-gutters">
+                  {imageArray[0] ? (
+                    <div className="offset-xs-0 offset-sm-4 col-sm-4 col-6">
+                      <div className="single-show-archive">
+                        <img
+                          className="ImageTyp101 hgtTp1"
+                          src={image_url + imageArray[0]}
+                          alt=""
+                        />
+                        <a href="index.html#">
+                          <i className="fa fa-link" aria-hidden="true"></i>
+                        </a>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {imageArray[1] ? (
+                    <div className="col-6 col-sm-4">
+                      <div className="single-show-archive">
+                        <img
+                          className="ImageTyp101 hgtTp1"
+                          src={image_url + imageArray[1]}
+                          alt=""
+                        />
+                        <a href="index.html#">
+                          <i className="fa fa-link" aria-hidden="true"></i>
+                        </a>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
 
-			<section className="show-archive">
-				{previous ? (
-					<div className="container">
-						<div className="d-flex justify-content-center row">
-							<div className="col-xl-10">
-								<div className="section-title style-five">
-									<h2>PREVIOUS SHOWS</h2>
-									<p>{previous.title?previous.title:""}</p>
-								</div>
-								<div className="show-archive-wrapper row no-gutters">
-									{imageArray[0]?(
-									<div className="offset-xs-0 offset-sm-4 col-sm-4 col-6">
-										<div className="single-show-archive">
-											<img src={image_url + imageArray[0]} alt="" />
-											<a href="index.html#"><i className="fa fa-link" aria-hidden="true"></i>
-											</a>
-										</div>
-									</div>):""}
-									{imageArray[1]?(
-									<div className="col-6 col-sm-4">
-										<div className="single-show-archive">
-											<img src={image_url + imageArray[1]} alt="" />
-											<a href="index.html#"><i className="fa fa-link" aria-hidden="true"></i>
-											</a>
-										</div>
-									</div>):""}
-								
-									{imageArray[0]?(
-										
-									<div className="col-xs-12 col-sm-8">
-									
-										<div className="single-show-archive video-archive">
-											<img src={image_url + imageArray[0]} alt="" />
-											<div className="video-player">
-
-												<div className="text">
-													<p>Show Stoppers Time</p>
-													<span>(Live in Tokyo, 2017)</span>
-												</div>
-
-											</div>
-										</div>
-									</div>):""}
-									{/* <div className="col-12 col-sm-4">
+                  {imageArray[2] ? (
+                    <div className="col-xs-12 col-sm-8">
+                      <div className="single-show-archive video-archive">
+                        <img
+                          className="ImageTyp101 hgtTp2"
+                          src={image_url + imageArray[2]}
+                          alt=""
+                        />
+                        <div className="video-player">
+                          <div className="text">
+                            <p>Show Stoppers Time</p>
+                            <span>(Live in Tokyo, 2017)</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {imageArray[0] ? (
+                    <div className="col-12 col-sm-4">
+                      <div className="single-show-archive video-archive">
+                        <img
+                          className="ImageTyp101 hgtTp3"
+                          src={image_url + imageArray[0]}
+                          alt=""
+                        />
+                        <div className="video-player">
+                          <div className="text">
+                            <p>Show Stoppers Time</p>
+                            <span>(Live in Tokyo, 2017)</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {/* <div className="col-12 col-sm-4">
 								<div className="single-show-archive">
 									<img src="media/album/21.jpg" alt=""/>
 									<a href="index.html#"><i className="fa fa-link" aria-hidden="true"></i>
 									</a>
 								</div>
 							</div> */}
-								</div>
-
-							</div>
-
-						</div>
-
-					</div>
-				) : ""}
-				{videoArray[1]?(<ModalVideo  title={videoArray[1]}/>):""}
-					
-
-			</section>
-
-		);
-	}
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+        {videoArray[1] ? <ModalVideo title={videoArray[1]} /> : ""}
+      </section>
+    );
+  }
 }
 
-
-
-
 const mapDispatchToProps = dispatch => {
-	// call action functions
-	return {
-		previousshow: () => dispatch(actionCreators.previousshow())
-	};
+  // call action functions
+  return {
+    previousshow: () => dispatch(actionCreators.previousshow())
+  };
 };
 
 const mapStateToProps = state => {
-	return {
-		previous: state.previousshow.items
-	};
+  return {
+    previous: state.previousshow.items
+  };
 };
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(previousShowHome);
-
-
-
-
-
-
