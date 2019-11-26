36cc2d8b3a3f078381e5740220de1736b3ce16c3
ijas <ijas.prof@gmail.com>
2019-11-26 16:56:54 +0400

    ijas

--- src/components/Pages/UpcomingShows/upcomingShows.jsx
+++ src/components/Pages/UpcomingShows/upcomingShows.jsx
@@ -1,38 +1,37 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import './style.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 import { connect } from "react-redux";
 import * as actionCreators from "../../../../src/store/actions/";
-import Moment from 'moment';
+import Moment from "moment";
 class upcomingShowHome extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       event: []
-
-    }
+    };
   }
 
   componentDidMount() {
-    this.props.fetchEventDetail()
+    this.props
+      .fetchEventDetail()
       .then(() => {
         const events = this.props.events;
-        var event = events.slice(0, 2)
+        var event = events.slice(0, 2);
         if (event) {
           this.setState({
             event: event
-          })
+          });
         }
       })
-      .catch((error) => {
+      .catch(error => {
         if (error) {
         }
-      })
+      });
   }
 
-
   render() {
-    const ticketFlag = localStorage.getItem('ticketheader');
+    const ticketFlag = localStorage.getItem("ticketheader");
     return (
       <section className="section-padding show-archive">
         <div className="container">
@@ -41,39 +40,47 @@ class upcomingShowHome extends Component {
               <div className="section-title style-four">
                 <h2>UPCOMING SHOWS</h2>
               </div>
-              {this.state.event && this.state.event.map(item => {
-                return (
-                  <div key={item.id} className="single-show-ticket row no-guters">
-                    <div className="col-sm-3 col-md-3 col-lg-3">
-                      <div className="date-time">
-                        <h6>{Moment(item.date_to).format('dddd')}</h6>
-                        <p>{Moment(item.date_to).format('DD-MM-YYYY')}</p>
+              {this.state.event &&
+                this.state.event.map(item => {
+                  return (
+                    <div
+                      key={item.id}
+                      className="single-show-ticket row no-guters"
+                    >
+                      <div className="col-sm-3 col-md-3 col-lg-3">
+                        <div className="date-time">
+                          <h6>{Moment(item.date_to).format("dddd")}</h6>
+                          <p>{Moment(item.date_to).format("DD-MM-YYYY")}</p>
+                        </div>
                       </div>
-
-                    </div>
-                    <div className="col-sm-6 col-md-6 col-lg-7">
-                      <h5>{item.title}</h5>
-                      <p> {item.location}</p>
-                    </div>
-                    <div className="col-sm-3 col-md-3 col-lg-2">
-                      <div className="a_hover">
-                        {ticketFlag === true ? (
-                          <Link className="tim-btn" to={{
-                            pathname: `/ticket-detail/${item.id}`
-                          }}>
-                            Ticket
-                                </Link>) : (<Link className="tim-btn">
-                            No Tickets Available Now
-                                </Link>)}
+                      <div className="col-sm-6 col-md-6 col-lg-7">
+                        <h5>{item.title}</h5>
+                        <p> {item.location}</p>
+                      </div>
+                      <div className="col-sm-3 col-md-3 col-lg-2">
+                        <div>
+                          {ticketFlag === true ? (
+                            <Link
+                              className="tim-btn"
+                              to={{
+                                pathname: `/ticket-detail/${item.id}`
+                              }}
+                            >
+                              Ticket
+                            </Link>
+                          ) : (
+                            <Link className="tim-btn BtnCst1">No Tickets</Link>
+                          )}
+                        </div>
                       </div>
                     </div>
-                  </div>);
-              })}
+                  );
+                })}
             </div>
           </div>
         </div>
       </section>
-    )
+    );
   }
 }
 
@@ -86,12 +93,8 @@ const mapDispatchToProps = dispatch => {
 
 const mapStateToProps = state => {
   return {
-    events: state.eventdetails.eventdetails,
+    events: state.eventdetails.eventdetails
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(upcomingShowHome);
-
+export default connect(mapStateToProps, mapDispatchToProps)(upcomingShowHome);
